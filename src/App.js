f3ad9ce71b80914661d2a8db2666b62e6a612de0
Jeremy <realwxp02@gmail.com>
2022-12-29 17:33:12 -0500

    section 10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+ 
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+ 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <MainNavigation />
+     <Routes>
+       <Route path="/" element={<Users/>}/>
+       <Route path="/places/new" element={<NewPlace/>}/>
+     </Routes>
+   </Router>
   );
-}
-
+};
+ 
 export default App;
