95a0703f937ed28b1fed41007701c9e30320b87e
Jeremy <realwxp02@gmail.com>
2023-01-02 00:47:08 -0500

    section 30

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -11,19 +11,51 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+
  
 const App = () => {
-  return (
-    <Router>
-     <MainNavigation />
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Routes>
+        <Route path="/" element={<Users/>}/>
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/places/new" element={<NewPlace />} />
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+      </Routes>
+    );
+
+  } else {
+    routes = (
       <Routes>
         <Route path="/" element={<Users/>}/>
         <Route path="/:userId/places" element={<UserPlaces/>}/>
-        <Route path="/places/new" element={<NewPlace/>}/>
-        <Route path="/places/:placeId" element={<UpdatePlace/>}/>
         <Route path="/auth" element={<Auth/>}/>
       </Routes>
-    </Router>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+          <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
    );
  };
   
