ceaa521e358463fbcb76958759f575f1e51286b6
Jeremy <realwxp02@gmail.com>
2023-01-01 23:42:59 -0500

    section 29

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import {
  
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UserPlaces from "./places/pages/UserPlaces";
  
 const App = () => {
   return (
-   <Router>
-    <MainNavigation />
-     <Routes>
-       <Route path="/" element={<Users/>}/>
-       <Route path="/:userId/places" element={<UserPlaces/>}/>
-       <Route path="/places/new" element={<NewPlace/>}/>
-       <Route path="/places/:placeId" element={<UpdatePlace/>}/>
-     </Routes>
-   </Router>
-  );
-};
- 
-export default App;
+    <Router>
+     <MainNavigation />
+      <Routes>
+        <Route path="/" element={<Users/>}/>
+        <Route path="/:userId/places" element={<UserPlaces/>}/>
+        <Route path="/places/new" element={<NewPlace/>}/>
+        <Route path="/places/:placeId" element={<UpdatePlace/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+      </Routes>
+    </Router>
+   );
+ };
+  
+ export default App;
